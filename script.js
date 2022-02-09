9ef10f99f3ead091de7b2314b236fa2bacbaf891
baitwell <julian.feinen@gmail.com>
2022-02-09 12:57:24 +0100

    a

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function auswerten()//wird beim drücken des Auswerte-Knopfes aktiviert
             }
         }
     }
-    if(AnzahlRichtigerLoesungen>2)//überprüfen ob mindestens 3 fragen richtig sind
+    if(AnzahlRichtigerLoesungen>2)//überprüft ob mindestens 3 fragen richtig sind
     {
         document.getElementsByClassName("ergebnis")[0].innerHTML = "<u>Bestanden!</u>";
     }
@@ -42,8 +42,8 @@ function holeLoesungen()
     //über z.b. AJAX wird der server angesprochen
     // der server liefert einen string mit den richtigen Optionen zurück
     // z.B. 
-    // 0-0 1-2
+    // 0-0;1-2....
     //soll heißen: in frage 1 ist die erste Option korrekt, in frage 2 die dritte Option
-    //die erste Ziffer "0"-0 stellt die erste frage dar. Die Zweite 0-"0", die zugehörige Option
-    return "0-0;1-1;2-2;3-0;4-1;5-2"; //das muster für die Korrekten Optionen lautet also 1,2,3,1,2,3
+    //die erste Ziffer "0"-0 stellt die frage dar. Die Zweite 0-"0", die zugehörige Option
+    return "0-0;1-1;2-2;3-0;4-1;5-2"; //das Muster für die Korrekten Optionen lautet also 1,2,3,1,2,3
 }
